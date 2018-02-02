873cf16d52970883dc77ae2ece2838cf1baa5af9
fadeela muhammad <fadilamuhammad400@gmail.com>
2018-02-02 16:23:03 +0000

    yeah

--- connect.php
+++ connect.php
@@ -1,3 +1,34 @@
 <?php
+$servername = getenv('IP');
+   
+ $username = getenv('C9_USER');
+    
+$password1 = "";
+   
+ $database = "c9";
+    
+$dbport = 3306;
+
+    // Create connection
+   
+ $db = new mysqli($servername, $username, $password1, $database, $dbport);
+
+    // Check connection
+    if ($db->connect_error) {
+        die("Connection failed: " . $db->connect_error);
+    }
+    
+    
+    $myQ = $db->query("select uname,password from login");
+   
+   if($myQ==1){
+       echo "login successfull";
+       header("location:welcome.php");
+   }
+   else{
+       echo"incorrect details";
+
+   }
+
 
 ?>
